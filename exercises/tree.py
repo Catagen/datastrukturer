0885671966c06d60ba06d06f75ecd2a00a1a302a
Szymon Stypa <shabo.o@hotmail.com>
2016-03-07 20:40:35 +0100

    Tests in progress

--- exercises/tree.py
+++ exercises/tree.py
@@ -24,27 +24,137 @@ class BinarySearchTree():
 
     def insert(self, key, value=None):
         """Lägg till en nod i trädet."""
-        pass
 
-    def lookup(self, key):
+        if key < self.key:
+            if self.left:
+                self.left.insert(key, value)
+            else:
+                self.left = BinarySearchTree(key, value)
+
+        elif key >= self.key:
+            if self.right:
+                self.right.insert(key, value)
+            else:
+                self.right = BinarySearchTree(key, value)
+
+
+    def lookup(self, key, parent=None, rp=False):
         """Sök efter noden med matchande key.
 
         Returnerar matchande noden eller None.
         """
-        pass
+
+        if key == self.key:
+            return parent if rp else self
+        elif key < self.key and self.left:
+            return self.left.lookup(key, parent=self, rp=rp)
+        elif key > self.key and self.right:
+            return self.right.lookup(key, parent=self, rp=rp)
+
+        return None
 
     def delete(self, key):
         """Radera noden med matchande key."""
-        pass
 
-    def traverse(self):
+        bovpappa = self.lookup(key, rp=True)
+        bov = self.lookup(key)
+
+        nybovpappa = None
+        nybov = None
+
+        if bov:
+
+            if bov.left:
+                current = bov.left
+
+                while current:
+
+                    if not current.right.right:
+                        nybovpappa = current
+                        nybov = current.right
+
+                    current = current.right
+
+                nybovpappa.right = nybov.left
+
+            elif bov.right:
+                current = bov.right
+
+                while current:
+                    if not current.left.left:
+                        nybovpappa = current
+                        nybov = current.left
+
+                nybovpappa.left = nybov.right
+
+            else:
+                if bovpappa:
+                    if bov == bovpappa.left:
+                        bovpappa.left = None
+                    elif bov == bovpappa.right:
+                        bovpappa.right = None
+                else:
+                    del self
+
+                return
+
+            nybov.left, nybov.right = bov.left, bov.right
+
+            if bovpappa:
+                if bov == bovpappa.left:
+                    bovpappa.left = nybov
+                elif bov == bovpappa.right:
+                    bovpappa.right = nybov
+            else:
+                self.key = nybov.key
+                self.left = nybov.left
+                self.right = nybov.right
+
+        else:
+            raise ValueError
+
+        return
+
+    def traverse(self, this=None):
         """En in-order traversering av trädets noder.
 
         Implementera som en generator.
         """
-        pass
+
+        if not this: this = self
+
+        if this.left:
+            for node in self.traverse(this.left):
+                yield node
+
+        yield this
+
+        if this.right:
+            for node in self.traverse(this.right):
+                yield node
+
 
     def __str__(self):
         """Utskrift av trädets alla noder (in-order)."""
         # Använd traverse...
-        pass
+
+        li = []
+
+        for node in self.traverse():
+            li.append(node.key)
+
+        return str(li)
+
+if __name__ == "__main__":
+
+    b = BinarySearchTree(12)
+    b.insert(3)
+    b.insert(10)
+    b.insert(1)
+    b.insert(0)
+    b.insert(3)
+    b.insert(5)
+    b.insert(20)
+    b.insert(18)
+    b.insert(21)
+    print(b)
--- tests/test_tree.py
+++ tests/test_tree.py
@@ -1,10 +1,22 @@
 import unittest
-from exercises.tree import Tree
-
-
-class TreeNodeTests(unittest.TestCase):
-    pass
-
+from exercises.tree import BinarySearchTree
 
 class TreeTests(unittest.TestCase):
-    pass
+
+    def setUp(self):
+        self.tree = BinarySearchTree(50)
+
+    def text_insert(self):
+        self.tree.insert(40)
+        self.tree.insert(30)
+        self.tree.insert(60)
+        self.tree.insert(70)
+        assertEqual([self.tree.left.left, self.tree.left, self.tree, self.right, self.right.right], self.tree.tranverse())
+
+    def test_lookup(self):
+        self.assertEqual(self.tree, self.tree.lookup(50))
+        self.tree.insert(40)
+        self.tree.insert(60)
+        self.assertEqual(self.tree.left, self.tree.lookup(40))
+        self.assertEqual(self.tree.right, self.tree.lookup(60))
+        self.assertEqual(None, self.tree.lookup(10))
